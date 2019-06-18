bf2234dac537297a8825eacf09667841ac38abd6
NightZ <835272261@qq.com>
2019-06-18 17:39:29 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,4 @@
 from .IO_handler import readfiles
 from .SBSTree import sbst
 from .Boolean import vector_encode, vector_decode, boolean_op
-from .index_cop import vb_decode, vb_encode
+from .index_cop import vb_decode, vb_encode, print_vb_code 
